3a33854b7d5d3dffc8e487138414e25cce652b1f
charrann12 <nethisaicharan12345@gmail.com>
2025-06-29 20:35:59 +0530

    day 11

---
+++ 06_StacknQueue/minStack.cpp
@@ -0,0 +1,53 @@
+/* MIN STACK
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+You must implement a solution with O(1) time complexity for each function.
+
+*/
+
+class MinStack {
+    stack<pair<int, int>>st;
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        int min;
+        if(st.empty()){
+            min = val;
+        }
+        else{
+            min = std::min(st.top().second,val);
+        }
+        st.push({val,min});
+    }
+    
+    void pop() {
+        st.pop();
+    }
+    
+    int top() {
+        return st.top().first;
+    }
+    
+    int getMin() {
+        return st.top().second;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
---
+++ 06_StacknQueue/validParanthesis.cpp
@@ -0,0 +1,48 @@
+/* VALID PARANTHESIS
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+
+Input: s = "()"
+
+Output: true
+
+Example 2:
+
+Input: s = "()[]{}"
+
+Output: true
+
+Example 3:
+
+Input: s = "(]"
+
+Output: false
+*/
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char ch : s){
+            if(ch == '('|| ch == '{' || ch == '['){
+                st.push(ch);
+            }
+            else{
+                if(st.empty()) return false;
+
+                char top = st.top();
+                if((ch ==')' && top!='(') || (ch == '}' && top!='{') || (ch==']' && top!='['))return false;
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
