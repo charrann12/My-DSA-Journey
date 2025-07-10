2659d9f56b035ba3bbfa02d9b5eea4af6734558f
charrann12 <nethisaicharan12345@gmail.com>
2025-07-10 19:30:34 +0530

    day 22

---
+++ 09_Binary_Trees/postOrder.cpp
@@ -0,0 +1,71 @@
+LEETCODE 94
+/*
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+Input: root = [1,null,2,3]
+
+Output: [1,3,2]
+
+Explanation:
+
+
+
+Example 2:
+
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+
+Output: [4,2,6,5,7,1,3,9,8]
+
+Explanation:
+
+
+
+Example 3:
+
+Input: root = []
+
+Output: []
+
+Example 4:
+
+Input: root = [1]
+
+Output: [1]
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        if(root == NULL) return {};
+    
+    vector<int> left = inorderTraversal(root->left);
+     ans.insert(ans.end(), left.begin(), left.end());
+
+ans.push_back(root->val);
+
+    vector<int> right = inorderTraversal(root->right);
+
+   
+    ans.insert(ans.end(), right.begin(), right.end());
+
+    return ans;
+
+    }
+};
