4a2b4f19278c783b7744cbdfec594d2de3c87247
charrann12 <nethisaicharan12345@gmail.com>
2025-07-22 20:31:27 +0530

    day 32

---
+++ 09_Binary_Trees/rightView.cpp
@@ -0,0 +1,39 @@
+// problem: https://leetcode.com/problems/binary-tree-right-side-view/description/
+// intuition: similar to level order traversal but small extension of that 
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        if(!root) return ans;
+
+        queue<TreeNode*>q;
+        q.push(root);
+
+        while(!q.empty()){
+            int size = q.size();
+            vector<int>level;
+            for(int i =0;i<size;i++){
+                TreeNode* node = q.front();
+                q.pop();
+                level.push_back(node->val);
+                 if(node->right) q.push(node->right);
+                if(node->left) q.push(node->left);
+               
+            }
+            ans.push_back(level[0]);
+        }
+        return ans;
+    }
+};
